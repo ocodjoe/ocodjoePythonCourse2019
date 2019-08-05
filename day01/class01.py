f7df7e00860bdb1d53fcd48ab93190788df8c34a
Oswald <37198307+ocodjoe@users.noreply.github.com>
2019-08-05 11:57:51 -0500

    Update class01.py

--- day01/class01.py
+++ day01/class01.py
@@ -57,17 +57,17 @@ print(5 - 2)
 print(5 * 2)
 print(5 ** 2) #this is 5 raised to the power 2. 
 print(5 / 2) ## in python2, this is floor division. #This gives you the float (run down)
-print(5 // 2) #this gives you the exact
+print(5 // 2) #this gives you the exact value.
 
 whole = 5//3
-remainder = 5%3
+remainder = 5%3. #this is modular arithmetic
 "Five divded by three is %d and %d fifths" % (whole, remainder)
 
 ## assignment is flexible
 five = 5
-five += 1
+five += 1 #adds 1 and sets five as the new value
 print(five)
-five /= 3
+five /= 3 #divides by 3 and sets five as the new value
 print(five)
 five -= 1
 print(five)
@@ -81,7 +81,7 @@ print(five)
 ## Add decimal to integer
 ## Be careful with the distinction here!
 
-print(12//5)
+print(12//5)  #note: float has decimals, others are just integers.
 print(12.0/5)
 print(float(12)/5)
 
@@ -93,10 +93,10 @@ print(float(12)/5)
 ## Lists will probably be your goto storage in Python, like vectors in R
 
 cohort = ["Oswald", "Jordan"]
-cohort.append("Ryan") ## add one element
+cohort.append("Ryan") ## add one element. #append is for adding a single element
 print(cohort)
 
-cohort.extend(["Ben", "Crystal"]) ## add a list
+cohort.extend(["Ben", "Crystal"]) ## add a list. #extend is for adding another list
 print(cohort)
 
 ## NOT like this...
@@ -104,11 +104,11 @@ print(cohort)
 
 ## remember our indexing... starts at 0!!
 print(cohort[0]) ## first
-print(cohort[-1]) ## last
+print(cohort[-1]) ## last.  #just note -1 means last element, -5 means fifth to last element, and so on.
 print(cohort[len(cohort)]) ## !!!
 
 ## lists are mutable
-cohort[0] = "Alexandra"
+cohort[0] = "Alexandra" 
 print(cohort)
 
 ## Insert by index
@@ -116,7 +116,7 @@ cohort.insert(2, "Lucas")
 print(cohort)
 
 ## Remove, but be careful
-last_element = cohort.pop()
+last_element = cohort.pop()  #so cohort.pop just elimnates the last element?
 print(cohort)
 print(last_element)
 
@@ -129,11 +129,11 @@ print(list_of_lists)
 
 
 ## Tuples ------------------------------------------------
-
+#Note: We use a tuple when we want to make a list such that no one can change it's elements.
 ## Like lists, but immutable
 ## Not super common....
 
-tup = (1,6,5, "Apple", ["python", "R"])
+tup = (1,6,5, "Apple", ["python", "R"]) #note: to make a tuple, you use a curly bracket. but square bracket for list.
 print(tup[1])
 
 tup[1] = 10000 ## !!!
@@ -153,11 +153,11 @@ tup.append(10000) ## !!!
 
 ryden_info = {"name" : "Ryden", "age" : 26, "research" : ["social media", "methods"]}
 
-print(ryden_info.keys())
-print(ryden_info.values())
+print(ryden_info.keys()) #this is like colnames in R
+print(ryden_info.values()) #gives the values
 
 ryden_info[0] ##!!!
-ryden_info["research"]
+ryden_info["research"] #so you index using the keys not indices as we know them. That's why we say it's not ordered
 
 ryden_info["last_name"] = "Butler"
 print(ryden_info)
@@ -169,9 +169,9 @@ print(ryden_info)
 ## Booleans and Conditionals ------------------------------------------------
 
 t = True
-f = False
-print(t == f)
-print(t != f)
+f = False 
+print(t == f) #
+print(t != f) #not equal
 
 
 ## Perform operation(s) if condition is met 
@@ -179,7 +179,7 @@ x = 2
 if x == 1:
 	print("x is one")
 elif x == 2:
-	print("x is two")
+	print("x is two")        #note: elif means else if.
 else:
 	print("x is neither one or two")
 
@@ -201,22 +201,22 @@ if x == 1:
 ## Loops ------------------------------------------------
 
 even_nums = []
-for i in range(1,10):
+for i in range(1,10):     #essentially we are populating even-nums with even numbers.
 	if i%2 == 0:
 		even_nums.append(i)
 
 hi = []
-for i in "hello":
+for i in "hello":          #essentially we are taking each character in hello and populating hi with it
 	hi.append(i)
 
 ## more succinct way to write simple loop
-bye = [i for i in "goodbye"]
+bye = [i for i in "goodbye"]          #this is the more efficient/pythonic way of doing loops. eg. those above.
 
 
-while len(even_nums) > 1:
-	print(even_nums.pop())
+while len(even_nums) > 1:         #note: a while loop is just a loop that keeps running as long as some condition is met.
+	print(even_nums.pop())    #so when doing a while loop, make sure there's some condition that will trigger a false
 
-print(even_nums)
+print(even_nums)                  #else your loop will run forever. 
 
 
 
