7171f2d4d6ead0c78dd3cc52d7991a526f4fbb61
Oswald Codjoe <ocodjoe@u.rochester.edu>
2019-08-11 12:45:30 -0500

    Update
    3 more methods to finish software.

--- homeworks/hw1/second attempt at hw1.py
+++ homeworks/hw1/second attempt at hw1.py
@@ -58,11 +58,35 @@ class Portfolio:
             return "You don't have sufficient funds to buy this stock."
    
     #cont. here. -- thus 4 more functions to go. before that add commoents to above done.    
-    def sellStock(self, share_qty, stock):
-        if stock.Symbol == self.Stock[1]:
-            return self.Stock[0]- share_qty
-        elif self.Stock[0] < share_qty:   
+    def sellStock(self, symbol, share_qty):
+        if len(self.Stock) == 0:
             return "You don't have sufficient stocks to sell."
+        
+        elif len(self.Stock) != 0 and len(self.Cash)== 0:
+            self.Stock.append([-int(share_qty),symbol])
+            allowable_stocks = ["HFH"]
+            price_of_allowables = [20]
+            for i in allowable_stocks:
+                if symbol == i:
+                    self.Cash.append(-int(share_qty*price_of_allowables[0]))
+                    return "You've sold %d shares of %s. And your cash balance is $%d." %(share_qty, symbol, self.Cash)
+                    
+        elif len(self.Stock)!= 0 and len(self.Cash) != 0:    
+            self.Stock.append([-int(share_qty),symbol])
+            allowable_stocks = ["HFH"]
+            price_of_allowables = [20]
+            for i in allowable_stocks:
+                if symbol == i:
+                    self.Cash[0] = self.Cash[0]+ int(share_qty*price_of_allowables[0])
+                    return "You've sold %d of %s stock. And your cash balance is %d." %(share_qty, symbol, self.Cash[0])
+                    #and your stock transaction history is ...where - number means selling and positive means buying.
+                #think of displaying stock operations by displaying self.Stock
+        
+        #I need to add one final bit to ensure that user can't sell more than what he has
+        #the first if takes care of that in some way but only for empty stocks.
+
+        
+        
     
     def buyMutualFund(self, share_qty, mutualfund):
         return "You've bought %d shares of %s." %(share_qty, mutualfund)
@@ -71,7 +95,8 @@ class Portfolio:
         return "You've sold %d share of %s." %(share_qty, mutualfund)
     
     def print(self):
-        return self.database
+        return {"Cash":self.Cash, "Stocks":self.Stock, "MutualFund":self.MutualFund}
+        #think of a way of 
         
     def history(self): 
         return "You've conducted %s so far." %(self)        
@@ -97,6 +122,8 @@ pfA.addCash(120)
 pfA.buyStock(5,s)
 pfA.buyStock(5,s)
 pfA.Stock
-
+pfA.sellStock('HFH',1)  #cont. here..
+pfA.Cash
 pfA.sellStock(2,s)
 
+pfA.print()
