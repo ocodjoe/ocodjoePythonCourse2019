06c60005cae73182debde69879b643b65fea933f
Oswald Codjoe <ocodjoe@u.rochester.edu>
2019-08-20 22:48:04 -0500

    Update
    Update

--- homeworks/hw3/hw3_second_oswald.py
+++ homeworks/hw3/hw3_second_oswald.py
@@ -27,28 +27,94 @@ for item in tweepy.Cursor(api.followers_ids,'@WUSTL').items(20):
 WashU_followers
 
 ##Determining who among the 20 WashU followers has the highest number of tweets
-WashU_followersstatuses =[]
+WashU_followersstatuses_count =[]
 for i in WashU_followers:
-    WashU_followersstatuses.append(api.get_user(i).statuses_count)
+    WashU_followersstatuses_count.append(api.get_user(i).statuses_count)
     
-WashU_followersstatuses 
+WashU_followersstatuses_count 
 
-max_value = max(WashU_followersstatuses)
-max_index = WashU_followersstatuses.index(max_value)
+max_value = max(WashU_followersstatuses_count)
+max_index = WashU_followersstatuses_count.index(max_value)
 
 api.get_user(WashU_followers[max_index]).name
 
 
 #QUESTION 1B 
+
 ##Determining who among the 20 WashU followers has the highest number of followers
-WashU_followers2 = []
+WashU_followers_count = []
+for i in WashU_followers:
+    WashU_followers_count.append(api.get_user(i).followers_count)
+    
+WashU_followers_count  
+
+max_value = max(WashU_followers_count)
+max_index = WashU_followers_count.index(max_value)
+
+api.get_user(WashU_followers[max_index]).name
 
 
+#QUESTION 1C
 
+##Determining who among the 20 that WashU follows has the highest number of followers.
+WashU_friends = []
+for item in tweepy.Cursor(api.friends_ids, '@WUSTL').items(20):
+    WashU_friends.append(item) 
 
+WashU_friends_followers_count = [] 
+for i in WashU_friends:
+    WashU_friends_followers_count.append(api.get_user(i).followers_count)
 
+WashU_friends_followers_count
 
+max_value = max(WashU_friends_followers_count)
+max_index = WashU_friends_followers_count.index(max_value)
 
+api.get_user(WashU_friends[max_index]).name
 
+#QUESTION 1D
 
+##Determining who among the 20 that WashU follows has the highest number of tweets by group.
 
+WashU_friends_statuses_count = []
+for i in WashU_friends:
+    WashU_friends_statuses_count.append(api.get_user(i).statuses_count)
+
+WashU_friends_statuses_count
+
+max_value = max(WashU_friends_statuses_count)
+max_index = WashU_friends_statuses_count.index(max_value)
+
+api.get_user(WashU_friends[max_index]).name #this just gives friend with max tweets
+
+#Grouping the 20 that WashU follows into layman, expert, and celebrity
+#But I got stuck so come back to this later
+
+layman = []
+expert = []
+celebrity = []
+    
+for i in WashU_friends_followers_count:
+    
+    if i < 100:
+        layman.append(i) 
+    elif 100 <= i <= 1000:
+        expert.append(i)
+    elif i > 1000:
+        celebrity.append(i)
+
+max_value1 = max(layman)
+max_index1 = layman.index(max_value1)
+
+api.get_user(WashU_friends[max_index1]).name
+api.get_user(WashU_friends[max_index1])    
+    
+layman
+expert
+celebrity
+#########################3    
+        
+
+##QUESTION 2A
+
+#
